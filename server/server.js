57182df3b18f0fbf0e712c09c5d03051c2a4771c
Shrishti Mishra <imshrishti0212@gmail.com>
2026-01-13 00:45:08 +0530

    Configure frontend for Render backend

--- server/server.js
+++ server/server.js
@@ -71,8 +71,8 @@ app.set("io", io);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    httpServer.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    httpServer.listen(process.env.PORT || 5000, () => {
+      console.log("Server running");
+    });
   })
   .catch((err) => console.log("❌ MongoDB Error:", err));
